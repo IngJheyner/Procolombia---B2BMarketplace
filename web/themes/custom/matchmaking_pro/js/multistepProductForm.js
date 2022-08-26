f5a7e036945d051a911f81deb6714493c6ab04ba
yasminOrj <yasmin@seedem.co>
2022-08-26 10:32:00 -0500

    Ajustes estilos checkbox y descripcion campo

--- web/themes/custom/matchmaking_pro/js/multistepProductForm.js
+++ web/themes/custom/matchmaking_pro/js/multistepProductForm.js
@@ -10,18 +10,18 @@
     Drupal.behaviors.matchmaking_pro = {
       attach: function(context, settings) {
         // Custom code here 
-        // Agregar clase a los pasos anteriores
-       
-        const currentStep = $("li.current");
-        currentStep.prevAll().addClass("completed");
-        const textCurrent = currentStep.wrapInner("<span class='title-step'></span>");
         
-        $(textCurrent).clone().appendTo(".cp-core-node-multistep-sidebar");
 
         //Cargar modales 
         $(context).find('body').once('legal-modal').each(function () {
-          //Creación de contenedor que envuel multipaso y formlario
-
+          
+          // Agregar clase a los pasos anteriores       
+          const currentStep = $("li.current");
+          currentStep.prevAll().addClass("completed");
+          const textCurrent = currentStep.wrapInner("<span class='title-step'></span>");        
+          $(textCurrent).clone().appendTo(".cp-core-node-multistep-sidebar");
+          
+          //Creación Variables del legal modal
           let modalFirstStep = document.getElementById('legal-modal');
           modalFirstStep.classList.add('modal');
           let showFirstStepModal = new bootstrap.Modal(modalFirstStep, {})
@@ -42,11 +42,12 @@
           modalContainer.appendChild(modalFooter);
           //Cargar modal primer paso cuando el formulario se abre la primera vez    
           document.addEventListener('load', function(e) {
-                    
               //Mostrar modal cuando la pagina se abre por primera vez y ocultar modal cual se da clic en aceptar
               if(window.location.hash !== "#cp-core-multistep-form" ) {
                   showFirstStepModal.show();
-                  hideModal(btnAgreeFirstSt,showFirstStepModal);					
+                  hideModal(btnAgreeFirstSt,showFirstStepModal);
+                  hideModal(btnCloseFirstSt,showFirstStepModal);
+                  document.body.classList.add('multistep-form')
               }else{
                 showFirstStepModal.hide();
               }                      
