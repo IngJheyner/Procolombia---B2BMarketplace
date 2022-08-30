1459624abc020543307ecf9df7e836cefcfccda2
yasminOrj <yasmin@seedem.co>
2022-08-30 07:35:58 -0500

    Ajustes Javascript pasos anteriores

--- web/themes/custom/matchmaking_pro/js/multistepProductForm.js
+++ web/themes/custom/matchmaking_pro/js/multistepProductForm.js
@@ -16,6 +16,45 @@
         $('#edit-field-categorization').SumoSelect();
         $('#edit-field-partida-arancelaria-tax').SumoSelect();
         $('#edit-field-pr-product-availability').SumoSelect();
+
+        //Cargar modales
+        $(context).find('body').once('.cp-core-multistep-form').each(function () {
+
+          // Agregar clase a los pasos anteriores
+          const currentStep = $("li.current");
+          currentStep.prevAll().addClass("completed");
+          const textCurrent = currentStep.wrapInner("<span class='title-step'></span>");
+          $(textCurrent).clone().appendTo(".cp-core-node-multistep-sidebar");
+
+          //Creación Variables del legal modal
+          let modalFirstStep = document.getElementById('generic-modal-legal-modal');
+          let showFirstStepModal = new bootstrap.Modal(modalFirstStep, {});
+          const btnCloseFirstSt = document.querySelector('.close');
+
+
+          hideModal(btnCloseFirstSt,showFirstStepModal)
+              function hideModal(btn, modal){
+                btn.addEventListener('click',()=>{
+                modal.hide();
+                })
+              }
+
+          //Cargar modal primer paso cuando el formulario se abre la primera vez
+          document.addEventListener('load', function(e) {
+            showFirstStepModal.show();
+              //Mostrar modal cuando la pagina se abre por primera vez y ocultar modal cual se da clic en aceptar
+              if(window.location.hash !== "#cp-core-multistep-form" ) {
+                  showFirstStepModal.show();
+                  hideModal(btnCloseFirstSt,showFirstStepModal);
+                  document.body.classList.add('multistep-form')
+              }else{
+                showFirstStepModal.hide();
+              }
+
+          }, true);
+
+        });
+
         
         // Open modal for all generic items.
         if ($('.generic-modal:not(.generic-modal-legal-modal)').length) {
@@ -102,46 +141,6 @@
           });
         }
 
-        
-        //Cargar modales
-        $(context).find('body').once('.cp-core-multistep-form').each(function () {
-
-          // Agregar clase a los pasos anteriores
-          const currentStep = $("li.current");
-          currentStep.prevAll().addClass("completed");
-          const textCurrent = currentStep.wrapInner("<span class='title-step'></span>");
-          $(textCurrent).clone().appendTo(".cp-core-node-multistep-sidebar");
-
-          //Creación Variables del legal modal
-          let modalFirstStep = document.getElementById('generic-modal-legal-modal');
-          let showFirstStepModal = new bootstrap.Modal(modalFirstStep, {});
-          const btnCloseFirstSt = document.querySelector('.close');
-
-
-          hideModal(btnCloseFirstSt,showFirstStepModal)
-              function hideModal(btn, modal){
-                btn.addEventListener('click',()=>{
-                modal.hide();
-                })
-              }
-
-          //Cargar modal primer paso cuando el formulario se abre la primera vez
-          document.addEventListener('load', function(e) {
-            showFirstStepModal.show();
-              //Mostrar modal cuando la pagina se abre por primera vez y ocultar modal cual se da clic en aceptar
-              if(window.location.hash !== "#cp-core-multistep-form" ) {
-                  showFirstStepModal.show();
-                  hideModal(btnCloseFirstSt,showFirstStepModal);
-                  document.body.classList.add('multistep-form')
-              }else{
-                showFirstStepModal.hide();
-              }
-
-          }, true);
-
-          
-        
-        });
       }
     };
   })(jQuery, Drupal);
