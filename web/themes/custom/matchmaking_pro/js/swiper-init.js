083a4a8578c20ddc491c6baa5c918f34d35beb37
Marco Castaneda <mcastaneda@seedem.co>
2022-08-30 07:44:43 -0500

    Especificando script que duplica imágenes

--- web/themes/custom/matchmaking_pro/js/swiper-init.js
+++ web/themes/custom/matchmaking_pro/js/swiper-init.js
@@ -3,7 +3,7 @@
 		attach: function(context, settings) {
 			let node = document.querySelector('.node--type-product');
 			
-			let imagesField = $('.field--name-field-images');
+			let imagesField = $('.node--type-product.node--view-mode-product-service-presave-preview .field--name-field-images');
 			imagesField.addClass('swiper-wrapper');
 			imagesField.children('.field__item').addClass('swiper-slide');
 			let fieldClone = imagesField.clone(true);
@@ -12,37 +12,26 @@
 			
 			imagesField.wrap('<div class="swiper swiper-main" />');
 			fieldClone.wrap('<div class="swiper swiper-thumbs" />');
-			/*
-			let fieldImage = node.querySelector('.field--name-field-images');
-			fieldImage.parentElement.classList.add('swiper');
-			// fieldImage.parentElement.classList.add('carousel-field-images');
-			fieldImage.classList.add('swiper-wrapper');
-			fieldImage.querySelectorAll('.field__item').forEach(e => {
-				e.classList.add('swiper-slide');
-			});
-			
-			let sliderTop = fieldImage.parentElement;
-			let sliderThumbs = sliderTop.cloneNode(true);
-			sliderTop.insertAdjacentHTML('afterend', sliderThumbs);*/
+
 
 			var swiper = new Swiper(".swiper-thumbs", {
-			        loop: true,
-			        spaceBetween: 10,
-			        slidesPerView: 4,
-			        freeMode: true,
-			        watchSlidesProgress: true,
-			      });
-			      var swiper2 = new Swiper(".swiper-main", {
-			        loop: true,
-			        spaceBetween: 10,
-			        navigation: {
-			          nextEl: ".swiper-button-next",
-			          prevEl: ".swiper-button-prev",
-			        },
-			        thumbs: {
-			          swiper: swiper,
-			        },
-			      });
+				loop: true,
+				spaceBetween: 10,
+				slidesPerView: 4,
+				freeMode: true,
+				watchSlidesProgress: true,
+			});
+			var swiper2 = new Swiper(".swiper-main", {
+				loop: true,
+				spaceBetween: 10,
+				navigation: {
+					nextEl: ".swiper-button-next",
+					prevEl: ".swiper-button-prev",
+				},
+				thumbs: {
+					swiper: swiper,
+				},
+			});
 
 			/*
 				Toggle button play || pause
