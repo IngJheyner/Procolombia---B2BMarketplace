d9842ef2b3729e772774cb16dc3bea4240906e2c
yasminOrj <yasmin@seedem.co>
2022-09-05 08:35:49 -0500

    Ajustes JS modal firstStep

--- web/themes/custom/matchmaking_pro/js/multistepProductForm.js
+++ web/themes/custom/matchmaking_pro/js/multistepProductForm.js
@@ -28,6 +28,7 @@
 
           //Creación Variables del legal modal
           let modalFirstStep = document.getElementById('generic-modal-legal-modal');
+            modalFirstStep.classList.remove('autoload');
           if (modalFirstStep) {
             let showFirstStepModal = new bootstrap.Modal(modalFirstStep, {});
             const btnCloseFirstSt = document.querySelector('.close');
@@ -40,7 +41,6 @@
             }
             //Cargar modal primer paso cuando el formulario se abre la primera vez
             document.addEventListener('load', function(e) {
-              showFirstStepModal.show();
                 //Mostrar modal cuando la pagina se abre por primera vez y ocultar modal cual se da clic en aceptar
                 if(window.location.hash !== "#cp-core-multistep-form" ) {
                     showFirstStepModal.show();
