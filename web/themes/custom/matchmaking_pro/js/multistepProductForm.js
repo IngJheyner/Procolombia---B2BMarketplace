99c934cf8dad70202921b00d4d631bb8f5630bc8
yasminOrj <yasmin@seedem.co>
2022-09-09 13:35:45 -0500

    ajustes estilos paso 2 a 4

--- web/themes/custom/matchmaking_pro/js/multistepProductForm.js
+++ web/themes/custom/matchmaking_pro/js/multistepProductForm.js
@@ -10,18 +10,7 @@
       attach: function(context, settings) {
         // Custom code here
 
-          //Agregar clase a campos obligatorios
-          $('.field--name-field-pr-country summary').addClass('js-form-required form-required');
-          $('.form-item-field-pr-terms-of-condition-value').addClass('js-form-required form-required');
-
-          //Tooltips interacciones
-          $('.form-control.is-invalid').after('<span class="tooltip-is-invalid"><p>Este campo es requerido</p></span>');
-          $('.lightbulb-tooltip').click(()=>{
-          $( '.lightbulb-tooltip span').toggle();
-         })
-        //Cargar modales
-        $(context).find('body').once('.cp-core-multistep-form').each(function () {
-          //Uso de Sumo select para personlizar campos tipo select
+        //Uso de Sumo select para personlizar campos tipo select
           $('#edit-field-product-type').SumoSelect({
             forceCustomRendering: true,
           });
@@ -43,6 +32,18 @@
               forceCustomRendering: true,
             }
           );
+          //Agregar clase a campos obligatorios
+          $('.field--name-field-pr-country summary').addClass('js-form-required form-required');
+          $('.form-item-field-pr-terms-of-condition-value').addClass('js-form-required form-required');
+
+          //Tooltips interacciones
+          $('.form-control.is-invalid').after('<span class="tooltip-is-invalid"><p>Este campo es requerido</p></span>');
+          $('.lightbulb-tooltip').click(()=>{
+          $( '.lightbulb-tooltip span').toggle();
+         })
+        //Cargar modales
+        $(context).find('body').once('.cp-core-multistep-form').each(function () {
+          
           $('.step_4 .js-form-type-managed-file.form-type-managed-file small.description').wrap('<div class="tooltip-img"></div>');
           // Agregar clase a los pasos anteriores
           const currentStep = $("li.current");
