5c99d585b0207b370c508e835911f24a25ee285a
yasminOrj <yasmin@seedem.co>
2022-09-13 13:41:01 -0500

    Ajustes estilos vista del producto

--- web/themes/custom/matchmaking_pro/js/swiper-init.js
+++ web/themes/custom/matchmaking_pro/js/swiper-init.js
@@ -2,39 +2,45 @@
 	Drupal.behaviors.viewSlides = {
 		attach: function(context, settings) {
 			let node = document.querySelector('.node--type-product');
-
-			let imagesField = $('.node--type-product.node--view-mode-product-service-presave-preview .field--name-field-images');
-
-			imagesField.addClass('swiper-wrapper');
-			imagesField.children('.field__item').addClass('swiper-slide');
-			let fieldClone = imagesField.clone(true);
-			
-			imagesField.wrap('<div class="carrusel-thumbs-wrapper" />');
-			fieldClone.insertAfter(imagesField);
-
-			imagesField.wrap('<div class="swiper swiper-main" />');
-			fieldClone.wrap('<div class="swiper swiper-thumbs" />');
-
-			$('<div class="swiper-button-prev"></div><div class="swiper-button-next"></div>').insertAfter('.swiper-main');
-
-			var swiper = new Swiper(".swiper-thumbs", {
-				loop: false,
-				spaceBetween: 5,
-				slidesPerView: 5,
-				freeMode: true,
-				watchSlidesProgress: true,
-			});
-			var swiper2 = new Swiper(".swiper-main", {
-				loop: true,
-				spaceBetween: 10,
-				slidesPerView: 1,
-				navigation: {
-					nextEl: ".swiper-button-next",
-					prevEl: ".swiper-button-prev",
-				},
-				thumbs: {
-					swiper: swiper,
-				},
+		
+			$(context).find('body').once('.node--type-product').each(function () {
+				let imagesField = $('.node--type-product.node--view-mode-product-service-presave-preview .field--name-field-images');
+				let prFullImgField = $('.node--type-product.node--view-mode-full .field--name-field-images');
+				addSwipper(prFullImgField);
+				addSwipper(imagesField);
+				function addSwipper(image){
+					image.addClass('swiper-wrapper');
+					image.children('.field__item').addClass('swiper-slide');
+					let fieldClone = image.clone(true);
+					
+					image.wrap('<div class="carrusel-thumbs-wrapper" />');
+					fieldClone.insertAfter(image);
+		
+					image.wrap('<div class="swiper swiper-main" />');
+					fieldClone.wrap('<div class="swiper swiper-thumbs" />');
+		
+					$('<div class="swiper-button-prev"></div><div class="swiper-button-next"></div>').insertAfter('.swiper-main');
+				}
+	
+				var swiper = new Swiper(".swiper-thumbs", {
+					loop: false,
+					spaceBetween: 5,
+					slidesPerView: 5,
+					freeMode: true,
+					watchSlidesProgress: true,
+				});
+				var swiper2 = new Swiper(".swiper-main", {
+					loop: true,
+					spaceBetween: 10,
+					slidesPerView: 1,
+					navigation: {
+						nextEl: ".swiper-button-next",
+						prevEl: ".swiper-button-prev",
+					},
+					thumbs: {
+						swiper: swiper,
+					},
+				});
 			});
 			
 			/*
