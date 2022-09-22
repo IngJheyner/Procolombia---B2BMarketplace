797a67955753b1bf720e864de7a0530d40f6959b
yasminOrj <yasmin@seedem.co>
2022-09-22 09:45:50 -0500

    Ajustes preview

--- web/themes/custom/matchmaking_pro/js/multistepProductForm.js
+++ web/themes/custom/matchmaking_pro/js/multistepProductForm.js
@@ -89,12 +89,7 @@
           $('.view-display-id-page_1 .form-select:not(#edit-field-categorization-target-id)').SumoSelect({
             forceCustomRendering: true,
           });
-          $('#edit-field-categorization-target-id').SumoSelect({
-            forceCustomRendering: true,
-            search:true,
-            searchText: Drupal.t('Search'),
-            noMatch: Drupal.t('No matches for "{0}"')
-          });
+          
           $('#edit-field-product-type').SumoSelect({
             forceCustomRendering: true,
           });
